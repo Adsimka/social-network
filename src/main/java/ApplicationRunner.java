f7f69ea6a9a27882f7d96c30c95af0de957ceed8
ArsenMac <arsenminnegulov@mail.ru>
2024-04-21 02:30:48 +0500

    Create ConnectionPool with constructor and filling application.xml for constructor

--- src/main/java/ApplicationRunner.java
+++ src/main/java/ApplicationRunner.java
@@ -9,7 +9,8 @@ public class ApplicationRunner
 {
     public static void main(String[] args) {
         var context = new ClassPathXmlApplicationContext("application.xml");
+
         var connectionPool = context.getBean("pool", ConnectionPool.class);
-        System.out.println(context.getBean("p3", ConnectionPool.class));
+        System.out.println(connectionPool);
     }
 }
--- src/main/java/database/pool/ConnectionPool.java
+++ src/main/java/database/pool/ConnectionPool.java
@@ -1,5 +1,30 @@
 package database.pool;
 
+import java.util.List;
+import java.util.Map;
+
 public class ConnectionPool
 {
+    private final String username;
+    private final Integer poolSize;
+    private final List<Object> args;
+    private final Map<String, Object> properties;
+
+
+    public ConnectionPool(String username, int poolSize, List<Object> args, Map<String, Object> properties) {
+        this.username = username;
+        this.poolSize = poolSize;
+        this.args = args;
+        this.properties = properties;
+    }
+
+    @Override
+    public String toString() {
+        return "ConnectionPool{" +
+                "username='" + username + '\'' +
+                ", poolSize=" + poolSize +
+                ", args=" + args +
+                ", properties=" + properties +
+                '}';
+    }
 }
