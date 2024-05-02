1fa2f32e09414269baa66eef00aab78bffc1e792
ArsenMac <arsenminnegulov@mail.ru>
2024-05-02 21:37:51 +0500

    Create my InjectBeanPostProcessor for using implements postProcessBeforeInitialization (my InjectBean annotation)

--- src/main/java/ApplicationRunner.java
+++ src/main/java/ApplicationRunner.java
@@ -9,7 +9,7 @@ public class ApplicationRunner
 {
     public static void main(String[] args) {
         try (var context = new ClassPathXmlApplicationContext("application.xml")) {
-            var connectionPool = context.getBean("pool", ConnectionPool.class);
+            var connectionPool = context.getBean("p", ConnectionPool.class);
             System.out.println(connectionPool);
 
             var userRepository = context.getBean("userRepository", UserRepository.class);
--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,23 +1,10 @@
 package repository;
 
+import bpp.InjectBean;
 import database.pool.ConnectionPool;
 
 public class UserRepository
 {
-    private final ConnectionPool connectionPool;
-
-    private UserRepository(ConnectionPool connectionPool) {
-        this.connectionPool = connectionPool;
-    }
-
-    public static UserRepository of(ConnectionPool connectionPool) {
-        return new UserRepository(connectionPool);
-    }
-
-    @Override
-    public String toString() {
-        return "UserRepository{" +
-                "connectionPool=" + connectionPool +
-                '}';
-    }
+    @InjectBean
+    private ConnectionPool connectionPool;
 }
