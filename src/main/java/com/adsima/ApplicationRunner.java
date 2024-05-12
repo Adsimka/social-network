ea48df5fba4762b4243ef8f7a6a0f09417205c3b
ArsenMac <arsenminnegulov@mail.ru>
2024-05-12 14:40:14 +0500

    Spring Boot for the project

--- src/main/java/com/adsima/ApplicationRunner.java
+++ src/main/java/com/adsima/ApplicationRunner.java
@@ -1,21 +1,12 @@
 package com.adsima;
 
-import com.adsima.spring.config.ApplicationConfiguration;
-import com.adsima.spring.database.pool.ConnectionPool;
-import com.adsima.spring.database.repository.CRUDRepository;
-import com.adsima.spring.database.repository.CompanyRepository;
-import com.adsima.spring.service.CompanyService;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class ApplicationRunner
 {
     public static void main(String[] args) {
-        try (var context = new AnnotationConfigApplicationContext(ApplicationConfiguration.class)) {
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool + "pool1");
-
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1L));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
