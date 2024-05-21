e03e318f35d9ce58ef3cc1ff2dd34ea822333aa7
ArsenMac <arsenminnegulov@mail.ru>
2024-05-22 00:09:58 +0500

    Special parametrs (Sort, Pageable)

--- src/main/java/com/adsima/spring/database/repository/UserRepository.java
+++ src/main/java/com/adsima/spring/database/repository/UserRepository.java
@@ -2,14 +2,21 @@ package com.adsima.spring.database.repository;
 
 import com.adsima.spring.database.entity.Role;
 import com.adsima.spring.database.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long>
 {
+    List<User> findTop3ByBirthDateBefore(LocalDate date, Sort sort);
+
+    List<User> findAllBy(Pageable pageable);
+
     @Query("select u from User u where u.firstname like %:firstname% " +
             "and u.lastname like %:lastname%")
     List<User> findAllBy(String firstname, String lastname);
--- src/test/java/com/adsima/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/adsima/spring/integration/database/repository/UserRepositoryTest.java
@@ -7,7 +7,11 @@ import com.adsima.spring.integration.annotation.IntegrationTest;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -18,6 +22,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserRepositoryTest
 {
     private final UserRepository userRepository;
+
+    @Test
+    void checkPageable() {
+        var sortBy = Sort.sort(User.class);
+        var pageableBy = PageRequest.of(1, 2, sortBy.by(User::getId));
+
+        List<User> users = userRepository.findAllBy(pageableBy);
+        assertThat(users).hasSize(2);
+    }
+
+    @Test
+    void checkSort() {
+        var sortBy = Sort.sort(User.class);
+        var sort = sortBy.by(User::getFirstname).and(sortBy.by(User::getLastname));
+
+        userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+    }
 
     @Test
     void checkUpdate() {
