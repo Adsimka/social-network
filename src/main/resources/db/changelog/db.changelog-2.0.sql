5f8577c05849bffabe3f6231fd0d0787729e9b24
Arseniy <sahalysyk02@mail.ru>
2024-05-26 14:52:15 +0500

    Using liquibase to automatically execute sql scripts

--- src/main/resources/db/changelog/db.changelog-2.0.sql
+++ src/main/resources/db/changelog/db.changelog-2.0.sql
@@ -1,8 +1,17 @@
+--liquibase formatted sql
+
+--changeset adsimka:1
 ALTER TABLE users
 ADD COLUMN create_at TIMESTAMP;
+
+--changeset adsimka:2
 ALTER TABLE users
 ADD COLUMN modified_at TIMESTAMP;
+
+--changeset adsimka:3
 ALTER TABLE users
 ADD COLUMN created_by VARCHAR(32);
+
+--changeset adsimka:4
 ALTER TABLE users
 ADD COLUMN modified_by VARCHAR(32);
