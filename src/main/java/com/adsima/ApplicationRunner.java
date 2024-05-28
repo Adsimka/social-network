e96d8adb6df8aaee5e670b8e7b3cd5cea2291ad3
Arseniy <sahalysyk02@mail.ru>
2024-05-28 19:38:21 +0500

    Start learning Spring Web

--- src/main/java/com/adsima/ApplicationRunner.java
+++ src/main/java/com/adsima/ApplicationRunner.java
@@ -6,8 +6,8 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class ApplicationRunner
-{
+public class ApplicationRunner {
+
     public static void main(String[] args) {
         var context = SpringApplication.run(ApplicationRunner.class, args);
     }
