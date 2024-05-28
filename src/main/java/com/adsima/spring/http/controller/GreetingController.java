63122b2a69c8d59de0118f68b0b0cfd507550d8b
Arseniy <sahalysyk02@mail.ru>
2024-05-28 23:18:15 +0500

    Parameters, Headers, Cookies, Pageable

--- src/main/java/com/adsima/spring/http/controller/GreetingController.java
+++ src/main/java/com/adsima/spring/http/controller/GreetingController.java
@@ -1,16 +1,24 @@
 package com.adsima.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/api/greeting")
 public class GreetingController {
 
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView,
+                              @RequestParam Integer age,
+                              @RequestHeader String accept,
+                              @PathVariable Integer id,
+                              @CookieValue("JSESSIONID") String jSessionId) {
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
+    @RequestMapping("/bye")
     public ModelAndView bye(ModelAndView modelAndView) {
         modelAndView.setViewName("greeting/bye");
         return modelAndView;
