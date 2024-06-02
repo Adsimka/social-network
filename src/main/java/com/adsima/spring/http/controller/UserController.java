ded790780201b5607e90e66c67b1884c70ee9715
Arseniy <sahalysyk02@mail.ru>
2024-06-02 23:25:16 +0500

    Creating users.html and filling the database with data

--- src/main/java/com/adsima/spring/http/controller/UserController.java
+++ src/main/java/com/adsima/spring/http/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.adsima.spring.http.controller;
 
+import com.adsima.spring.database.entity.Role;
 import com.adsima.spring.dto.UserCreateEditDto;
 import com.adsima.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
--- src/main/java/com/adsima/spring/service/UserService.java
+++ src/main/java/com/adsima/spring/service/UserService.java
@@ -1,13 +1,11 @@
 package com.adsima.spring.service;
 
-import com.adsima.spring.database.entity.User;
 import com.adsima.spring.database.repository.UserRepository;
 import com.adsima.spring.dto.UserCreateEditDto;
 import com.adsima.spring.dto.UserReadDto;
 import com.adsima.spring.mapper.UserCreateEditMapper;
 import com.adsima.spring.mapper.UserReadMapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
